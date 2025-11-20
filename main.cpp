5e4d1140b4773a0a02dd5fc6cc306be451a11183
Luis Amaro <luisamlo559@gmail.com>
2025-11-19 22:43:07 -0800

    Decreased max amount of selectable parts. Added code to the build root choice. Made root choices easier to see with multiple asterisks.

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ if they wish to remove or add parts or can be used to cancel the order entirely.
 #include <limits>
 using  namespace std;
 
-const int tempLimit = 50;
+const int tempLimit = 30;
 
 class ComputerData
 {
@@ -62,11 +62,11 @@ int main()
 		cout << "Will you be ordering parts, building a a full PC, or reading in an order/build file?" << endl
 			<< "Type the matching number to proceed." << endl << "1. Order" << endl << "2. Build"
 			<< endl << "3. Read file" << endl;cin >> option;
-		if (option == 1) // Order root choice
+		if (option == 1) // Order root choice ***********************************
 		{
 			int partChoice;
 			cout << "Type number of which part(s)/action you would like to select:" << endl;
-			cout << "1. RAM(Random Access Memory)" << endl << "2. CPU (Central Processing Unit)"
+			cout << "1. RAM sticks (Random Access Memory)" << endl << "2. CPU (Central Processing Unit)"
 				<< endl << "3. GPU (Graphics Processing Unit)" << endl << "4. PSU (Power Supply Unit)" 
 				<< endl << "4. Motherboards" << endl << "5. Storage Devices" << endl << "6. Computer Fans"
 				<< endl << "7. Cords/Wires" << endl << "8. View cart" << endl << "9. To proceed to checkout" 
@@ -88,14 +88,15 @@ int main()
 				{
 					cout << "Returning to main menu.\n";
 					breakLoop = false;
+					totalNumParts = 0;
 					break;
 				}
 
-				if (partChoice == 1)
+				if (partChoice == 1) // RAM sticks
 				{
-					// RAM
+					
 				}
-				else if (partChoice == 2) // CPU
+				else if (partChoice == 2) // CPUs
 				{
 				
 				}
@@ -121,7 +122,11 @@ int main()
 				}
 				else if (partChoice == 8) //Cart
 				{
-
+					/* Will be used once parts are able to be added
+					for (int i = 0; i < totalNumParts; i++) {
+				CD[i].printItems();
+			}
+					*/
 				}
 				else if (partChoice == 9) // Checkout
 				{
@@ -138,21 +143,106 @@ int main()
 					<< endl << "4. Motherboards" << endl << "5. Storage Devices" << endl << "6. Computer Fans"
 					<< endl << "7. Cords/Wires" << endl << "8. View cart" << endl << "9. To proceed to checkout"
 					<< endl << "0. To cancel order and return to menu" << endl;
+				cout << totalNumParts << " parts selected" << endl;
 				cin >> partChoice;
-			}
-			if (partChoice == 0)
-			{
-				cout << "Returning to main menu" << endl;
-				breakLoop = false;
 			}
 
 		}
-		else if (option == 2) //Build root choice
+		else if (option == 2) //Build root choice **************************
 		{
-			
-			breakLoop = true;
+			int partChoice;
+			cout << "Type number of which part(s)/action you would like to select:" << endl;
+			cout << "1. RAM sticks (Random Access Memory)" << endl << "2. CPU (Central Processing Unit)"
+				<< endl << "3. GPU (Graphics Processing Unit)" << endl << "4. PSU (Power Supply Unit)"
+				<< endl << "4. Motherboards" << endl << "5. Storage Devices" << endl << "6. Computer Fans"
+				<< endl << "7. Cords/Wires" << endl << "8. View current build" << endl << "9. To proceed to checkout"
+				<< endl << "0. To cancel build and return to menu" << endl;
+			cout << "You can choose a max amount of 30 parts" << endl;
+			cout << "You will need to have selected a required amount of parts to be able to proceed to checkout" << endl;
+			cin >> partChoice;
+			while (true)
+			{
+				if (cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout << "Invalid input! Please enter a number.\n";
+					cin >> partChoice;
+					continue;
+				}
+
+				if (partChoice == 0) // exit
+				{
+					cout << "Returning to main menu.\n";
+					breakLoop = false;
+					totalNumParts = 0;
+					break;
+				}
+
+				if (partChoice == 1) // RAM sticks
+				{
+
+				}
+				else if (partChoice == 2) // CPUs
+				{
+
+				}
+				else if (partChoice == 3) // GPUs
+				{
+
+				}
+				else if (partChoice == 4)// PSUs
+				{
+
+				}
+				else if (partChoice == 5) // Motherboards
+				{
+
+				}
+				else if (partChoice == 6) // Storage devices
+				{
+
+				}
+				else if (partChoice == 7) // Cords/Wires
+				{
+
+				}
+				else if (partChoice == 8) //Build
+				{
+					// to do 
+				}
+				else if (partChoice == 9) // Checkout
+				{
+					/* This if will be used to prevent user from proceeding to checkout
+					if ()
+					{
+					
+					}
+					else
+					{
+					cout << "Not enough parts selected" << endl;
+					}
+					*/
+					breakLoop = true;
+					break; //To do add a method to checkout built pc. Temp break used
+				}
+				else
+				{
+					cout << "Invalid choice! Try again.\n";
+				}
+				cout << "Type number of which part(s)/action you would like to select:" << endl;
+				cout << "1. RAM sticks (Random Access Memory)" << endl << "2. CPU (Central Processing Unit)"
+					<< endl << "3. GPU (Graphics Processing Unit)" << endl << "4. PSU (Power Supply Unit)"
+					<< endl << "4. Motherboards" << endl << "5. Storage Devices" << endl << "6. Computer Fans"
+					<< endl << "7. Cords/Wires" << endl << "8. View current build" << endl << "9. To proceed to checkout"
+					<< endl << "0. To cancel build and return to menu" << endl;
+				cout << "You can choose a max amount of 30 parts" << endl;
+				cout << totalNumParts << " parts selected" << endl;
+				//to do add a way to display which parts are still required to check out
+				cin >> partChoice;
+			}
 		}
-		else if (option == 3) //File read root choice
+		else if (option == 3) //File read root choice ***********************************
 		{
 			ifstream infile;
 			cout << "Enter the file path of the file you wish to read:" << endl;
